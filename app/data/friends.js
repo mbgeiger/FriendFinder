1bbddd94eaeddad1a282a14ca8ceface693ca952
renatolaranja <matthew.b.geiger@gmail.com>
2019-02-23 15:32:06 -0500

    added routes and other stuff

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+//array of friends
+
+var friends = [{
+    name: "Drew Carey",
+    photo: "",
+    scores: [3, 3, 5, 4, 6, 5, 4, 9, 2, 1]
+  }, {
+    name: "Bob Barker",
+    photo: "",
+    scores: [2, 1, 3, 4, 3, 6, 1, 3, 2, 6]
+  }, {
+    name: "Pat Sajack",
+    photo: "",
+    scores: [3, 3, 5, 2, 1, 2, 2, 4, 1, 1]
+  },{
+    name: "Kelly Kapowski",
+    photo: "",
+    scores: [1, 2, 2, 1, 4, 1, 2, 3, 3, 3]
+  }, {
+    name: "Zack Morris",
+    photo: "",
+    scores: [5, 5, 5, 3, 2, 3, 2, 1, 3, 5]
+  }, {
+    name: "AC Slater",
+    photo: "",
+    scores: [2, 1, 3, 2, 5, 2, 3, 3, 4, 4]
+  }, {
+    name: "Jessie Spano",
+    photo: "",
+    scores: [3, 2, 2, 3, 4, 2, 5, 3, 1, 4]
+  }];
+  
+  module.exports = friends;
